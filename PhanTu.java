a6c1ca7f7eb5eb5071ca888bcdef741ea28254bb
nguyentung158 <78079771+nguyentung158@users.noreply.github.com>
2021-11-10 13:00:58 +0700

    Update PhanTu.java

--- PhanTu.java
+++ PhanTu.java
@@ -1,21 +1,28 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 
-
-
 public class PhanTu<T> {
 	private T t;
+	
+	public void setPhanTu(T t) {
+		this.t = t;
+	}
+	
+	public T getPhanTu() {
+		return t;
+	}
+	
+	public PhanTu(T t) {
+		// TODO Auto-generated constructor stub
+		this.t = t;
+	}
 	
 	public boolean isGreaterThan(T a) {
 		return ((Comparable<T>) t).compareTo(a) > 0;
